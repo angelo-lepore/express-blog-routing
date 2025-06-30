5d0e4acfea4a6ccf96cf4fc23381b32ea22916d9
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-30 15:39:30 +0200

    Added Express server and defined routes for image blog

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 3020;
 
+// importiamo router
+const posts_router = require("./routers/posts");
+
 // mettiamo il server in ascolto
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
@@ -15,3 +18,6 @@ app.get("/", (req, res) => {
 
 // configuriamo gli asset statici
 app.use(express.static("public"));
+
+// indichiamo con use che esistono nuove rotte
+app.use("/posts", posts_router);
